eecb7f606eecffc90c97420f756c3d3989c5822f
Mike Pinkerton <pinkerton@gmail.com>
2024-01-16 10:30:47 -0500

    Initial Commit

---
+++ SectionDeletionCrash/SectionDeletionCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SectionDeletionCrashApp.swift
+//  SectionDeletionCrash
+//
+//  Created by Mike Pinkerton on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SectionDeletionCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
